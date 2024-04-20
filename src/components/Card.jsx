db1e31053303fe3fb73b47d7b5c45c1f62aac6d5
hafiznurrahman <hafiznurrahman.dev@gmail.com>
2024-04-20 16:53:41 +0700

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,22 @@
+export default function Card({ number, link, image, title, description }) {
+    return (
+        <a
+            href={link}
+            className="w-full h-auto bg-gray-50 border-2 border-gray-900 shadow-[2px_2px_0_#000] shadow-gray-900 rounded-3xl p-2 relative transition-all duration-500 hover:scale-95"
+            translate="no"
+        >
+            <div className="w-8 h-8 flex justify-center items-center text-gray-50 font-bold bg-blue-500 shadow-[2px_2px_0_#000] shadow-blue-900 absolute -top-3 -left-3 rounded-full select-none">
+                {number}
+            </div>
+            <img
+                src={image}
+                alt={title || "image"}
+                className="min-w-full h-auto aspect-video bg-gray-200 shadow-[2px_2px_0_#000] shadow-gray-500 rounded-xl select-none"
+            />
+            <h2 className="font-black capitalize line-clamp-2 my-2">{title || "title"}</h2>
+            <small className="line-clamp-5">
+                {description || "description"}
+            </small>
+        </a>
+    );
+}
---
+++ src/components/Skill.jsx
@@ -0,0 +1,22 @@
+export default function Skill({ image, text, color }) {
+    return (
+        <div
+            className="flex justify-center items-center transition-all duration-500 hover:translate-x-1.5"
+            translate="no"
+        >
+            <img
+                src={image}
+                alt={text}
+                className="w-11 h-11 bg-gray-900 aspect-square rounded-lg select-none"
+            />
+            <div className="h-8 flex justify-center items-center bg-gray-50 rounded-r-lg p-2">
+                <span
+                    className="text-xs font-black uppercase drop-shadow-[0.5px_0.5px_0_#000]"
+                    style={{ color: color }}
+                >
+                    {text}
+                </span>
+            </div>
+        </div>
+    );
+}
